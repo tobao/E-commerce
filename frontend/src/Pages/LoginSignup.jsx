0a80f05336c40c49aca15b06580b36a77541f353
BaoTo <tobao97@gmail.com>
2024-05-25 19:41:53 +0700

    Responsive LoginSignup

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -6,7 +6,7 @@ const LoginSignup = () => {
       <div className="loginsignup-container">
         <h1>Sign Up</h1>
         <div className="loginsignup-fields">
-          <input type="text" placeholder='Your Name...' />
+          <input type="text" placeholder='Your Name...'/>
           <input type="email" placeholder='Email Address...' />
           <input type="password" placeholder='Password...' />
         </div>
